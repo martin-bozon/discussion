b919aa8f9ebc13c316785caed7ca25c859adb0bd
Martin-Bozon <62134476+Martin-Bozon@users.noreply.github.com>
2020-06-04 12:12:45 +0200

    site a jour (pour l'instant)

--- include/php_discussion.php
+++ include/php_discussion.php
@@ -8,20 +8,19 @@
             $connexionbd = mysqli_connect("localhost" , "root" , "" , "discussion");
 
             if(isset($_POST["validcom"]) && !empty($_POST["msg_"]))
-            {
-                $msg = addslashes($_POST["msg_"]);
-                $id = $_SESSION["id"];
-                
-                $insert_msg = "INSERT INTO messages (message, id_utilisateur, date) VALUES ('$msg', '$id', NOW())";
-                $query_insert = mysqli_query($connexionbd, $insert_msg);
-                mysqli_close($connexionbd);
-                header("Location:discussion.php");
-            }
+                {
+                    $msg = addslashes($_POST["msg_"]);
+                    $id = $_SESSION["id"];
+                    
+                    $insert_msg = "INSERT INTO messages (message, id_utilisateur, date) VALUES ('$msg', '$id', NOW())";
+                    $query_insert = mysqli_query($connexionbd, $insert_msg);
+                    mysqli_close($connexionbd);
+                    header("Location:discussion.php");
+                }
 
             $requete_msg = "SELECT messages.id, message, date, utilisateurs.login FROM messages INNER JOIN utilisateurs ON messages.id_utilisateur = utilisateurs.id ORDER BY id DESC";
             $query_requete_msg = mysqli_query($connexionbd, $requete_msg);
             $info_msg = mysqli_fetch_all($query_requete_msg, MYSQLI_ASSOC);
-
 
             foreach($info_msg as $message)
                 {
